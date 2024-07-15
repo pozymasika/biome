515d72e0abf520b4c070fe56b973ff845aec732b
Polycarp Masika <masikapolycarp@gmail.con>
2024-07-15 21:01:02 +0300

    add biome

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,22 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count] = useState(0);
+	const [count] = useState(0);
 
-  useEffect(() => {
-    console.log("the count is: ", count);
-  }, [count]);
+	useEffect(() => {
+		console.log("the count is: ", count);
+	}, [count]);
 
-  const text = ["hello", "my very good friend", "how are you?"];
-  const splitTextintoWords = text.map((sentence) => sentence.split(" ")).flat();
-  console.log(splitTextintoWords);
+	const text = ["hello", "my very good friend", "how are you?"];
+	const splitTextintoWords = text.flatMap((sentence) => sentence.split(" "));
+	console.log(splitTextintoWords);
 
-  return (
-    <section>
-      <p> this is an unformatted string</p>
-      <input
-        type="text"
-        autoFocus
-      />
-    </section>
-  );
+	return (
+		<section>
+			<p> this is an unformatted string</p>
+			<input type="text" autoFocus />
+		</section>
+	);
 }
 
 export default App;
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,8 @@
 const nums = [1, 2, 3, 4, 5];
 
 nums.map((num) => {
-  console.log("in loop:", num);
-  return num;
+	console.log("in loop:", num);
+	return num;
 });
 
 const sumNums = nums.reduce((acc, num) => acc + num, 0);
