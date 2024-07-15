aa5e8b4dfa2e2357c1954f5c5a5780e79bcdc648
Polycarp Masika <masikapolycarp@gmail.con>
2024-07-15 20:37:50 +0300

    prettier + eslint

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count] = useState(0);
+
+  useEffect(() => {
+    console.log("the count is: ", count);
+  }, [count]);
+
+  const text = ["hello", "my very good friend", "how are you?"];
+  const splitTextintoWords = text.map((sentence) => sentence.split(" ")).flat();
+  console.log(splitTextintoWords);
+
+  return (
+    <section>
+      <p> this is an unformatted string</p>
+      <input
+        type="text"
+        autoFocus
+      />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/util.ts
@@ -0,0 +1,9 @@
+const nums = [1, 2, 3, 4, 5];
+
+nums.map((num) => {
+  console.log("in loop:", num);
+  return num;
+});
+
+const sumNums = nums.reduce((acc, num) => acc + num, 0);
+console.log("sum_nums:", sumNums);
